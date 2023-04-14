442bba59e3c046fac16ccd51ba79f1b36c3ffcc4
efbonneville <e.f.bonneville@lumc.nl>
2023-04-14 21:47:03 +0200

    Testing different targets structures

--- R/one-replication.R
+++ R/one-replication.R
@@ -1,14 +1,3 @@
-source(here("R/data-generation.R"))
-source(here("R/smcfcs.finegray.R"))
-source(here("R/crprep-timefixed.R"))
-
-args_event_times <- list(
-  mechanism = "correct_FG",
-  params = tar_read(true_params_correct_FG),
-  censoring_type = "none"
-)
-args_missingness <- list(mech_params = list("prob_missing" = 0.1, "mechanism_expr" = "Z"))
-args_imputations <- list(m = 4, iters = 1, rjlimit = 1000) #note rjlimit does not matter for binary X
 
 
 # Add helper functions here --
@@ -26,7 +15,6 @@ extract_FGR_essentials <- function(FGR_fit,
   base_cuminc <- drop(predict(FGR_fit, newdata = newdat_baseline, times = timepoints))
 
   # Note could ask for crr object? Not very heavy, only 10% of total FGR size
-  # Make this into a dataframe!!
   essentials <- data.table(
     "time" = timepoints,
     "base_cuminc" = base_cuminc,
@@ -41,7 +29,7 @@ extract_FGR_essentials <- function(FGR_fit,
 }
 
 # Start here
-# Argument scenario ids?
+# Argument scenario ids? or pred times??
 one_replication <- function(args_event_times,
                             args_missingness,
                             args_imputations) {
@@ -147,8 +135,9 @@ one_replication <- function(args_event_times,
   #pred_times <- round(seq(0, 10, by = 1 / 12), digits = 3)
   pred_times <- 1:10 # or every 6 months??
 
-  timepoints <- pred_times
-  FGR_fit <- mod_CCA
+  # Editttt
+  #timepoints <- pred_times
+  #FGR_fit <- mod_CCA
 
   # Fit models in each imputed dataset
   nested_impdats[, mods := .(
@@ -184,159 +173,13 @@ one_replication <- function(args_event_times,
     summaries_impdats
   )
 
-  # Add some scenario identifiers?
+  # Add some scenario identifiers
+  method_summaries[, scen_summary := list(args_event_times)]
 
   # Eventually check if too heavy..
   return(method_summaries)
-  #return(method_summaries[, .(summary = list(summary)), by = method])
 }
 
-
-# morisot pooling
-# https://github.com/survival-lumc/CauseSpecCovarMI/blob/master/R/illustrative-analysis-helpers.R
-
-test_imps <- replicate(
-  n = 4,
-  expr = {
-    one_replication(
-      args_event_times,
-      args_missingness,
-      args_imputations
-    )
-  },
-  simplify = FALSE
-)
-
-test_imps
-df <- rbindlist(test_imps, idcol = "sim_rep")
-
-# Bind coefs togeths
-df_coefs <- rbindlist(
-  with(df, Map(cbind, method = method, sim_rep = sim_rep, coefs_summary)),
-  fill = TRUE
-)
-
-# Bind predictions
-df_preds <- rbindlist(
-  with(df, Map(cbind, method = method, sim_rep = sim_rep, preds_summary)),
-  fill = TRUE
-)
-df_preds[is.na(imp), imp := 0]
-df_preds
-
-# Try test pooling
-new_pat <- c("X" = 0.5, "Z" = 1)
-new_pats <- list(
-  "A" = c("X" = 0.5, "Z" = 1),
-  "B" = c("X" = 0.5, "Z" = 0.5)
-)
-testo <- df_preds[, .(
-  pred = 1 - (1 - base_cuminc)^exp(drop(unlist(coefs) %*% new_pat))
-), by = c("method", "sim_rep", "time", "imp")]
-
-df_preds[, .(
-  lapply(new_pats, function(x) {
-    1 - (1 - base_cuminc)^exp(drop(unlist(coefs) %*% x))
-  })
-), by = c("method", "sim_rep", "time", "imp")]
-
-#for (j in cols) set(dt, j = j, value = -dt[[j]])
-# for predictions?
-
-# See also https://stackoverflow.com/questions/16846380/apply-a-function-to-every-specified-column-in-a-data-table-and-update-by-referen
-# or just rbindlist the very big ones..
-testo
 
 inv_cloglog <- function(x) 1 - exp(-exp(x))
 cloglog <- function(x) log(-log(1 - x))
-
-testo[, .(
-  pooled_pred = inv_cloglog(mean(cloglog(pred)))
-), by = c("method", "sim_rep", "time")] |>
-  ggplot(aes(time, pooled_pred)) +
-  geom_line(aes(col = method, linetype = method), size = 1) +
-  facet_wrap(~ sim_rep) +
-  theme_bw()
-
-essentials[, .(drop(unlist(coefs) %*% x)), by = c("time", "base_cuminc")]
-
-#df[, unlist(summary), by = c("method", "sim_rep")]
-
-# file:///C:/Users/efbonneville/Downloads/Manuscript%20(1).pdf
-df_coefs <- df[, .(summary_coefs = list(summary[[1]][[2]])), by = c("method", "sim_rep")]
-df_preds <- df[, .(summary_preds = list(summary[[1]][[1]])), by = c("method", "sim_rep")]
-
-# Save the preds as a df!
-
-
-df_coefs[, cbind(method, rbindlist(summary_coefs, fill = TRUE)), by = method]
-rbindlist(df_coefs[, cbind(method, sim_rep, summary_coefs)])
-df_coefs[, .(list(cbind(method, sim_rep, summary_coefs))), by = c("method", "sim_rep")]
-
-test <- df_coefs[1, ]
-test[, .(list(cbind(method, sim_rep, summary_coefs[[1]])))]
-
-rbindlist(Map(cbind, timestamp = df_coefs$method, df_coefs$summary_coefs), fill = TRUE)
-
-# Thank the lawddddddd https://stackoverflow.com/questions/58563899/dataframe-with-nested-dataframes-how-to-set-id-column-with-data-tablerbindli
-rbindlist(df_coefs[, .(Map(cbind, method, sim_rep, summary_coefs))], fill = TRUE)
-
-df_coefs[, .(
-  list(cbind(method, sim_rep, summary_coefs[[1]]))
-), by = c("method", "sim_rep")]
-
-unnest_fill <- function(dt, col, id) {
-  stopifnot(is.data.table(dt))
-  by <- substitute(id)
-  col <- substitute(col[[1]])
-}
-
-unnest_dt <- function(dt, col, id){
-  stopifnot(is.data.table(dt))
-  by <- substitute(id)
-  #col <- substitute(unlist(col, recursive = FALSE))
-  dt[, eval(col), by = eval(by)]
-}
-
-
-
-tidyr::unnest(df_coefs, summary_coefs)
-tidyr::unnest(df_preds, summary_preds)
-
-unnest_dt <- function(dt, col, id){
-  stopifnot(is.data.table(dt))
-  by <- substitute(id)
-  col <- substitute(unlist(col, recursive = FALSE))
-  dt[, eval(col), by = eval(by)]
-}
-
-
-unnest_dt <- function(tbl, col) {
-  tbl <- as.data.table(tbl)
-  col <- ensyms(col)
-  clnms <- syms(setdiff(colnames(tbl), as.character(col)))
-  tbl <- as.data.table(tbl)
-  tbl <- eval(
-    expr(tbl[, as.character(unlist(!!!col, recursive = T)), by = list(!!!clnms)])
-  )
-  colnames(tbl) <- c(as.character(clnms), as.character(col))
-  tbl
-}
-
-
-unnest_dt(df_coefs, summary_coefs)
-
-df_coefs[, unlist(summary_coefs, recursive = FALSE), c("method", "sim_rep")]
-
-
-df[, .(dasummary[[1]][[2]]), by = c("method", "sim_rep")]
-rbindlist()
-df[, .(pooled_info = summary[[1]][[2]]), by = c("method", "sim_rep")]
-pryr::object_size(df)
-pryr::mem_used()
-# https://win-vector.com/2014/05/30/trimming-the-fat-from-glm-models-in-r/
-
-
-# Use to get baseline cumulative incidence at grid of monthly timepoints
-1 - (1 - predict(mod_full, newdata = list(X_obs = 0, Z = 0),
-                 times = c(3, 4, 5)))^exp(mod_full$crrFit$coef[1])
--- _targets.R
+++ _targets.R
@@ -10,6 +10,7 @@ source(here("packages.R"))
 source(here("R/data-generation.R"))
 source(here("R/smcfcs.finegray.R"))
 source(here("R/crprep-timefixed.R"))
+source(here("R/one-replication.R"))
 # or
 #functions <- list.files(here("R"), full.names = TRUE)
 #invisible(lapply(functions, source))
@@ -19,19 +20,97 @@ source(here("R/crprep-timefixed.R"))
 # See https://books.ropensci.org/targets/hpc.html#future for parallelizing
 plan(callr)
 
+tar_option_set(error = "null")
+
 #tar_option_set(packages = project_pkgs)
 
 # use variance = FALSE in FGR to make it faster?
 
 
 # Or maybe only part for the LFPs (without censoring bit)
+#scenarios_actual <-
 scenarios <- expand.grid(
   "failure_time_model" = c("correct_FG", "misspec_FG"),
   "prob_space_domin" = c("low_p" = 0.25, "high_p" = 0.75),
-  "censoring_type" = c("none", "exponential", "uniform")
+  "censoring_type" = c("none"),#, "exponential", "uniform"),
+  stringsAsFactors = FALSE
 )
 
+dgm_params <- tar_map(
+  values = list("p" = scenarios$prob_space_domin[!duplicated(scenarios$prob_space_domin)]),
+  tar_target(
+    true_params_correct_FG,
+    list(
+      "cause1" = list(
+        "formula" = ~ X + Z,
+        "betas" = c(0.75, 0.5),
+        "p" = p,
+        "base_rate" = 1,
+        "base_shape" = 0.75
+      ),
+      "cause2" = list(
+        "formula" = ~ X + Z,
+        "betas" = c(0.75, 0.5), # pick some that result in non-zero cause-spec coefs!!
+        "base_rate" = 1, # might need to increase this? nah probs wont matter
+        "base_shape" = 0.75
+      )
+    )
+  ),
+  # Need to do this for large and small "p" (domination of prob space)
+  # Can probably to some sort of apply to vector of p
+  tar_target(
+    largedat_correct_FG,
+    generate_dataset(
+      n = 1e3,#1e6,
+      args_event_times = list(
+        mechanism = "correct_FG",
+        params = true_params_correct_FG,
+        censoring_type = "none"
+      ),
+      args_missingness = list(mech_params = list("prob_missing" = 0))
+    )
+  ),
+  tar_target(
+    params_weibull_lfps,
+    recover_weibull_lfps(
+      large_dat = largedat_correct_FG,
+      params_correct_FG = true_params_correct_FG
+    )
+  ),
+  tar_target(
+    largedat_weibull_cause_spec,
+    generate_dataset(
+      n = 1e3,#1e6,
+      args_event_times = list(
+        mechanism = "misspec_FG",
+        params = params_weibull_lfps,
+        censoring_type = "none"
+      ),
+      args_missingness = list(mech_params = list("prob_missing" = 0))
+    )
+  ),
+  tar_target(
+    largedat_weibull_FG_lfps,
+    recover_fg_lps(largedat_weibull_cause_spec)
+  )
+)
 
+sim_fun <- tar_map_rep(
+  name = all_sims,
+  values = scenarios, # What we are iterating over
+  command = {
+    eval(parse(text = paste0("true_params_correct_FG_", prob_space_domin)))
+  },
+  reps = 2,
+  batches = 1,
+  combine = TRUE#,
+  #names = c("failure_time_model", "censoring_type")
+)
+
+list(
+  dgm_params,
+  sim_fun
+)
 
 # Start of pipeline, iterate over scenarios df
 # list(
@@ -57,65 +136,4 @@ scenarios <- expand.grid(
 #     )
 #   )
 # )
-
-# First get it working for one p
-list(
-  tar_target(
-    true_params_correct_FG,
-    list(
-      "cause1" = list(
-        "formula" = ~ X + Z,
-        "betas" = c(0.75, 0.5),
-        "p" = 0.25,
-        "base_rate" = 1,
-        "base_shape" = 0.75
-      ),
-      "cause2" = list(
-        "formula" = ~ X + Z,
-        "betas" = c(0.75, 0.5), # pick some that result in non-zero cause-spec coefs!!
-        "base_rate" = 1, # might need to increase this? nah probs wont matter
-        "base_shape" = 0.75
-      )
-    )
-  ),
-  # Need to do this for large and small "p" (domination of prob space)
-  # Can probably to some sort of apply to vector of p
-  tar_target(
-    largedat_correct_FG,
-    generate_dataset(
-      n = 1e6,
-      args_event_times = list(
-        mechanism = "correct_FG",
-        params = true_params_correct_FG,
-        censoring_type = "none"
-      ),
-      args_missingness = list(mech_params = list("prob_missing" = 0))
-    )
-  ),
-  tar_target(
-    params_weibull_lfps,
-    recover_weibull_lfps(
-      large_dat = largedat_correct_FG,
-      params_correct_FG = true_params_correct_FG
-    )
-  ),
-  tar_target(
-    largedat_weibull_cause_spec,
-    generate_dataset(
-      n = 1e6,
-      args_event_times = list(
-        mechanism = "misspec_FG",
-        params = params_weibull_lfps,
-        censoring_type = "none"
-      ),
-      args_missingness = list(mech_params = list("prob_missing" = 0))
-    )
-  ),
-  tar_target(
-    largedat_weibull_FG_lfps,
-    recover_fg_lps(largedat_weibull_cause_spec)
-  )
-  # 'True' least false in cause-spec.. should depend on censoring?
-  # Also make scenarios table..
-)
 
