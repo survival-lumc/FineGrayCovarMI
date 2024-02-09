046129be489fdffbffde88ba070685ca20c2704e
edbonneville <e.f.bonneville@lumc.nl>
2024-02-09 10:44:08 +0100

    Pilot ran

--- _targets.R
+++ _targets.R
@@ -225,7 +225,8 @@ applied_imp_settings <- list(
 
 # Reminder total number is batches/reps
 applied_example <- list(
-  tar_target(applied_dat_raw, data.table(readRDS("data-raw//dat_clean.rds"))),
+  tar_target(file, "data-raw//dat_clean.rds", format = "file"),
+  tar_target(applied_dat_raw, data.table(readRDS(file))),
   tar_target(applied_dat, process_applied_dat(applied_dat_raw)),
   tar_rep(
     applied_impdats,
